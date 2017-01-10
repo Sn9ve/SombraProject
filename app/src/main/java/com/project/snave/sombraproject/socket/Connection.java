1f028b7b99296c71a418bb54eafe5b43d6e1cbd6
Sn9ve <Sn9ve@users.noreply.github.com>
2017-01-10 19:03:54 +0100

    socket V1.2 future change in progress V2

--- app/src/main/java/com/project/snave/sombraproject/socket/Connection.java
+++ app/src/main/java/com/project/snave/sombraproject/socket/Connection.java
@@ -1,44 +1,62 @@
 package com.project.snave.sombraproject.socket;
 
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
+import java.net.UnknownHostException;
 
 /**
- * Created by Snave on 07/01/2017.
+ * Created by Snave on 29/12/2016.
  */
-public class Connection implements Runnable {
+public class Connection extends Thread{
+    private String IP;
+    private short PORT;
+    public static Socket socket;
 
-    private Socket socket;
-    public static Thread TRANSMISSION_THREAD, RECEPTION_THREAD;
+    public Transmit TRANSMISSION_THREAD;
+    public Receive RECEPTION_THREAD;
     private DataOutputStream out = null;
     private BufferedReader in = null;
 
-    public Connection(Socket socket){
-        this.socket = socket;
+    private static Connection instance = new Connection();
+
+    private Connection() {
+        this.socket = null;
+        this.IP = "192.168.0.20";
+        this.PORT = 23;
+    }
+
+    public static Connection getInstance() {
+        return instance;
     }
 
     public void run() {
         try {
+            System.out.println("Demande de connexion");
+            socket = new Socket(IP, PORT);
+            // Si le message s'affiche c'est que je suis connecté
+            System.out.println("Connexion établie avec le serveur");
+
             out = new DataOutputStream(socket.getOutputStream());
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            TRANSMISSION_THREAD = new Thread(new Transmit(out));
+            /** Thread de transmission des données du joystick **/
+            TRANSMISSION_THREAD = new Transmit(out);
             TRANSMISSION_THREAD.start();
-            RECEPTION_THREAD = new Thread(new Receive(in));
+            /** Thread de réception des messages reçus du serveur **
+            RECEPTION_THREAD = new Receive(in);
             RECEPTION_THREAD.start();
+            */
 
+        } catch (UnknownHostException e) {
+            System.err.println("Impossible de se connecter à l'adresse "+ socket.getLocalAddress());
         } catch (IOException e) {
-            System.err.println("Server does not respond ");
+            System.err.println("Aucun serveur à l'écoute du port "+socket.getLocalPort());
         }
+    }
+
+    public void addToSendQueue(byte x, byte y){
+        TRANSMISSION_THREAD.addToSendQueue(x, y);
     }
 }
